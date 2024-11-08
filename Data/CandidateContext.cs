de991283ff8d7f553c7e92e117e3aa1d64f2f7ed
khadkamanish92 <khadkamanish92@gmail.com>
2024-11-08 20:32:58 +0545

    dbcontext added for candidata model

---
+++ Data/CandidateContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CandidateApi.Models;
+
+namespace CandidateApi.Data
+{
+    public class CandidateContext : DbContext
+    {
+        public CandidateContext(DbContextOptions<CandidateContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
