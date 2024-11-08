1937cbe8db81372ae4c3a1578c8185ebf1eba4a2
khadkamanish92 <khadkamanish92@gmail.com>
2024-11-08 20:38:14 +0545

    Repository with the interface layer made for candidate

---
+++ Repositories/CandidateRepository.cs
@@ -0,0 +1,45 @@
+﻿using CandidateApi.Data;
+using CandidateApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApi.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly CandidateContext _context;
+
+        public CandidateRepository(CandidateContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> CreateOrUpdateCandidateAsync(Candidate candidate)
+        {
+            var existingCandidate = await _context.Candidates
+                .FirstOrDefaultAsync(c => c.Email == candidate.Email);
+
+            if (existingCandidate != null)
+            {
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.TimeIntervalToCall = candidate.TimeIntervalToCall;
+                existingCandidate.LinkedInProfile = candidate.LinkedInProfile;
+                existingCandidate.GitHubProfile = candidate.GitHubProfile;
+                existingCandidate.Comment = candidate.Comment;
+
+                await _context.SaveChangesAsync();
+                return existingCandidate;
+            }
+            else
+            {
+                _context.Candidates.Add(candidate);
+                await _context.SaveChangesAsync();
+                return candidate;
+            }
+        }
+    }
+}
---
+++ Repositories/ICandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using CandidateApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CandidateApi.Repositories
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> CreateOrUpdateCandidateAsync(Candidate candidate);
+
+    }
+}
