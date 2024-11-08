9547b1b79b509f2035318ae22f8f271a8f993844
khadkamanish92 <khadkamanish92@gmail.com>
2024-11-08 19:51:54 +0545

    modified model so that required fields are checked

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,15 +1,30 @@
-﻿namespace CandidateApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateApi.Models
 {
     public class Candidate
     {
-        public int Id { get; set; }
+        public int Id { get; set; }               // Primary Key
+
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; }
+
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }          // Unique Identifier
+
         public string TimeIntervalToCall { get; set; }
+
         public string LinkedInProfile { get; set; }
+
         public string GitHubProfile { get; set; }
+
+        [Required(ErrorMessage = "Comment is required.")]
         public string Comment { get; set; }
     }
 }
