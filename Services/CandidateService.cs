9b6eff98b8ae41b1f1836e143afb57bb5d60e5ff
khadkamanish92 <khadkamanish92@gmail.com>
2024-11-08 20:35:31 +0545

    implemented method to create or update candidate data

---
+++ Services/CandidateService.cs
@@ -0,0 +1,23 @@
+﻿using CandidateApi.Models;
+using CandidateApi.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CandidateApi.Services
+{
+    public class CandidateService
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public CandidateService(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+        }
+
+        public async Task<Candidate> CreateOrUpdateCandidateAsync(Candidate candidate)
+        {
+            return await _candidateRepository.CreateOrUpdateCandidateAsync(candidate);
+        }
+
+    }
+}
